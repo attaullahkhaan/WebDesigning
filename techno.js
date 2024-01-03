90795aaa9749a7f7491e785333750515f6cc1fce
Engr. Atta Ullah Khan <attaullahkhan43878@gmail.com>
2024-01-03 15:09:10 +0500

    Create techno.js

---
+++ techno.js
@@ -0,0 +1,10 @@
+let light= “green”;
+if(light == "green") {
+ console.log("Drive")
+} else if (light == "orange") {
+ console.log("Get ready")
+} else if (light == "red") {
+ console.log("Dont' drive")
+} else {
+ console.log("The car is not green, orange, or red");
+}
